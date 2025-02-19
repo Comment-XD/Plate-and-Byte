bf2acef458db3b7d6c532499c1d87b9e53c1fe92
Branden Chen <90992234+Comment-XD@users.noreply.github.com>
2025-02-18 22:03:46 -0500

    Added Login and Signup Page

--- app.py
+++ app.py
@@ -2,26 +2,94 @@ from flask import (
     Flask,
     render_template,
     jsonify,
-    request
+    request,
+    redirect,
+    url_for
 )
 
 import pandas as pd
+import csv
+import os
+
+# from src.Restaurant import Restaurant
+# from src.Manager import Manager
+# from src.Employee import Employee
+USER_CSV_PATH = "data/users.csv"
+
+def check_credential(username, password):
+    user_df = pd.read_csv(USER_CSV_PATH, index_col=0)
+    if ((user_df.username == username) & (user_df.password == int(password))).any(): 
+        return True
+    
+    return False
+
+def add_user(username, password, role="user"):
+    if not os.path.exists(USER_CSV_PATH):
+        with open(USER_CSV_PATH, mode="w") as file:
+            file.write("username,password,role\n")
+
+    with open(USER_CSV_PATH, mode="a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([username, password, role])
+
+
+def user_exists(username):
+    user_df = pd.read_csv(USER_CSV_PATH)
+    if (user_df["username"] == username).any():
+        return True
+    
+    return False
+
+
 
 app = Flask(__name__)
+    
+# --------Root Path----------- #
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        if check_credential(username, password):
+            return redirect(url_for("home"))
+        
+        else:
+            return render_template("login.html", 
+                                   error="That Plate and Byte account doesn't exist. Enter a different account.")
+    
+    return render_template("login.html")
 
-# root path
-@app.route("/")
-def root():
-    return render_template("index.html")
+# --------- Signup Page --------- #
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        if user_exists(username, password):
+            return render_template("signup.html",
+                                   error="User already exists")
+        
+        else:
+            add_user(username, password)
+            return render_template("login.html")
+    
+    return render_template("signup.html")
+
+# ----------- Home Page ----------------- #
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    return render_template("home.html")
+    
+# Route for adding tables to grid
+@app.route("/restaraunt", methods=["POST"])
+def set_restaurant_dim():
+    return
 
 # Route for adding tables to grid
 @app.route("/add_tables", methods=["POST"])
 def add_tables():
-    return
-
-# Route changing the sidebar for access of employee and manager
-@app.route("/sidebar", methods=["POST"])
-def activate_sidebar():
     return
 
 # Route for the changing the pov of employee and manager
@@ -31,4 +99,4 @@ def change_pov():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
